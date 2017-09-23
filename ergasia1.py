8aa93e605220b9c418c7501095016aa1c2d61c44
epause <32221849+epause@users.noreply.github.com>
2017-09-23 17:41:44 +0300

    Create ergasia1.py

---
+++ ergasia1.py
@@ -0,0 +1,26 @@
+
+a=[1,2,3]
+b=[1,1,1,1,1]
+c=[2,3]
+d=[4,1]
+
+suma=sumb=sumc=sumd=0
+
+for num in a
+	suma += num
+	
+for num in b
+	sumb += num
+	
+for num in c
+	sumc += num
+	
+for num in d
+	sumd += num
+	
+newlist=[suma,sumb,sumc,sumd]
+
+max(newlist)
+min(newlist)
+
+print max(newlist)-min(newlist)
